8daf2ca9e8b33a7de301f0828e3ea1a1bff909c1
Gorka29 <gorkaruizaraujo@gmail.com>
2024-10-25 20:33:01 +0200

    rutas del menu con sus componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, InicioComponent, SobreMiComponent, ProyectosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'sobre-mi', component: SobreMiComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '' }
+];
