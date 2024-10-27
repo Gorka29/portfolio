960ff6d845ac4d903ada0eb90a31e550e072b111
Gorka29 <gorkaruizaraujo@gmail.com>
2024-10-27 21:59:27 +0100

    Añadiendo experiencia

--- src/app/experiencia/experiencia.component.ts
+++ src/app/experiencia/experiencia.component.ts
@@ -16,7 +16,7 @@ export class ExperienciaComponent {
       titulo: 'Desarrollador front-end',
       empresa: 'NexTret',
       descripcion: 'Migración de web con JSP a Angular y Java. Migración de aplicación Excel a web con Angular y Java. Gestión de proyectos con Atlassian (Jira y Bitbucket) y GIT. Uso de metodologías ágiles (Scrum, Kanban, Agile).',
-      tecnologias: ['Angular', 'Java', 'Jira', 'Bitbucket', 'GIT', 'Scrum', 'Kanban']
+      tecnologias: ['Angular', 'Typescript', 'Spring Boot', 'SCSS', 'Chroome DevTools', 'MySQL', 'Jira', 'Bitbucket', 'GIT', 'Scrum', 'Kanban']
     },
     {
       fechaInicio: 'DIC 2022',
@@ -24,7 +24,7 @@ export class ExperienciaComponent {
       titulo: 'Desarrollador full-stack',
       empresa: 'Didaktiker',
       descripcion: 'Desarrollo y diseño de aplicaciones internas utilizando Angular y Spring Boot. Gestión de proyectos con Atlassian (Jira y Bitbucket) y GIT. Metodologías ágiles (Scrum, Kanban, Agile).',
-      tecnologias: ['Angular', 'Spring Boot', 'Jira', 'Bitbucket', 'GIT', 'Scrum', 'Kanban']
+      tecnologias: ['Angular', 'Spring Boot', 'Typescript', 'PHP', 'SCSS', 'Chroome DevTools', 'MySQL', 'Jira', 'Bitbucket', 'GIT', 'Scrum', 'Kanban']
     },
     {
       fechaInicio: 'DIC 2021',
@@ -32,7 +32,7 @@ export class ExperienciaComponent {
       titulo: 'Desarrollador full-stack',
       empresa: 'Altia',
       descripcion: 'Creación y diseño del sistema de Bolsa Nacional de Empleo (BNE) de Chile utilizando Thymeleaf y Spring MVC. Desarrollo de otros proyectos con Angular y Spring Boot. Gestión de proyectos con Dedalo y SVN. Uso de metodologías ágiles (Scrum, Kanban, Agile).',
-      tecnologias: ['Thymeleaf', 'Spring MVC', 'Angular', 'Spring Boot', 'Dedalo', 'SVN', 'Scrum', 'Kanban']
+      tecnologias: ['Angular', 'Typescript', 'Javascript','Spring Boot', 'Spring MVC', 'Thymeleaf', 'SCSS', 'Chroome DevTools', 'MySQL', 'Dedalo', 'SVN', 'Scrum', 'Kanban']
     },
     {
       fechaInicio: 'OCT 2020',
@@ -40,7 +40,15 @@ export class ExperienciaComponent {
       titulo: 'Desarrollador full-stack',
       empresa: 'PKF Attest',
       descripcion: 'Realización y diseño de diversos proyectos utilizando Salesforce y tecnologías como Angular y Spring Boot. Gestión de proyectos con GIT.',
-      tecnologias: ['Salesforce', 'Angular', 'Spring Boot', 'GIT']
+      tecnologias: ['Salesforce', 'Angular', 'Typescript', 'Javascript', 'Spring Boot', 'SCSS', 'Chroome DevTools', 'MySQL', 'Scrum', 'GIT']
+    },
+    {
+      fechaInicio: 'FEB 2020',
+      fechaFin: 'JUN 2020',
+      titulo: 'Desarrollador full-stack',
+      empresa: 'Versia',
+      descripcion: 'Desarrollo de aplicaciones web y moviles para clientes de la empresa como Eroski. Gestión de proyectos con GIT.',
+      tecnologias: ['HTML', 'CSS', 'Javascript', 'JQuery', 'Bootstrap', 'MySQL','GIT']
     }
   ];
 
