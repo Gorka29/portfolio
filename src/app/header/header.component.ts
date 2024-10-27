cb8f1347de8fe90ce0bad8f86777675325e3666a
Gorka29 <gorkaruizaraujo@gmail.com>
2024-10-27 14:08:17 +0100

    seccion actual en el menu sticky

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { RouterOutlet, RouterModule, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +12,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   menuAbierto: boolean = false;
 
@@ -25,6 +28,59 @@ export class HeaderComponent {
 
   onSectionClick(sectionId: string) {
     this.sectionSelected.emit(sectionId);
+  }
+
+  seccionActual: string = 'Inicio';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.actualizarSeccionActual();
+    });
+    // Estas dos líneas hacen lo siguiente:
+
+    // 1. gsap.registerPlugin(ScrollTrigger);
+    // Esta línea registra el plugin ScrollTrigger con GSAP (GreenSock Animation Platform).
+    // ScrollTrigger es una herramienta poderosa que permite crear animaciones basadas en el scroll.
+    // Al registrarlo, se habilita su uso en toda la aplicación.
+
+    // 2. this.setupScrollTriggers();
+    // Esta línea llama al método setupScrollTriggers() de este componente.
+    // Este método probablemente configura los disparadores de scroll para las diferentes secciones de la página,
+    // permitiendo actualizar la sección actual basándose en la posición del scroll.
+
+    gsap.registerPlugin(ScrollTrigger);
+    this.setupScrollTriggers();
+  }
+
+  actualizarSeccionActual() {
+    const rutaActual = this.router.url.split('/')[1] || 'inicio';
+    this.seccionActual = this.capitalizarPrimeraLetra(rutaActual);
+  }
+
+  setupScrollTriggers() {
+    const secciones = ['inicio', 'sobre-mi', 'experiencia', 'contacto'];
+
+    secciones.forEach(seccion => {
+      ScrollTrigger.create({
+        trigger: `#${seccion}`,
+        start: 'top center',
+        end: 'bottom center',
+        onEnter: () => this.actualizarTextoSeccion(seccion),
+        onEnterBack: () => this.actualizarTextoSeccion(seccion)
+      });
+    });
+  }
+
+  actualizarTextoSeccion(seccion: string) {
+    this.seccionActual = this.capitalizarPrimeraLetra(seccion);
+  }
+
+  capitalizarPrimeraLetra(texto: string): string {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
   }
 
 }
