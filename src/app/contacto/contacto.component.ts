add39e667c81bf389f7efa55cfc5dce7c117dccf
Gorka29 <gorkaruizaraujo@gmail.com>
2024-10-27 21:45:01 +0100

    primera toma de contacto con el componente de contacto

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.scss'
 })
 export class ContactoComponent {
+  nombre: string = '';
+  email: string = '';
+  mensaje: string = '';
 
+  manejarEnvio() {
+    console.log('Formulario enviado', {
+      nombre: this.nombre,
+      email: this.email,
+      mensaje: this.mensaje
+    });
+    // Aquí puedes agregar la lógica para enviar el formulario
+  }
 }
