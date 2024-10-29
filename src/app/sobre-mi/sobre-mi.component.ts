0d2094bfda8f9ee38e2e42b3c293b3d8eff3a45a
Gorka29 <gorkaruizaraujo@gmail.com>
2024-10-29 21:25:11 +0100

    Cambio de la seccion actual con sticky menus anidados

--- src/app/sobre-mi/sobre-mi.component.ts
+++ src/app/sobre-mi/sobre-mi.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { SharedService } from '../services/shared.service'; // Asumiendo que tienes un servicio compartido
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mi',
@@ -8,32 +7,6 @@ import { SharedService } from '../services/shared.service'; // Asumiendo que tie
   templateUrl: './sobre-mi.component.html',
   styleUrl: './sobre-mi.component.scss'
 })
-export class SobreMiComponent implements OnInit {
-  @ViewChild('sobreMiSection') sobreMiSection!: ElementRef;
+export class SobreMiComponent {
 
-  constructor(private sharedService: SharedService) {}
-
-  ngOnInit() {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.sharedService.setCurrentSection('SOBRE MÍ');
-          }
-        });
-      },
-      { threshold: 0.5 } // Ajusta este valor según necesites
-    );
-
-    // Observamos cuando el componente esté listo
-    setTimeout(() => {
-      if (this.sobreMiSection) {
-        observer.observe(this.sobreMiSection.nativeElement);
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    // Limpieza del observer si es necesario
-  }
 }
