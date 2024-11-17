29058ab05cfb67cddf1720bfc1fb127a06dceff7
Gorka29 <gorkaruizaraujo@gmail.com>
2024-11-17 22:41:09 +0100

    error .ball GSAP arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,32 +42,32 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     // Configuración inicial del trazo y la bola
-    gsap.set("#text-path", { drawSVG: "0%" }); // Inicialmente oculta el trazo
-    gsap.set(".ball", { xPercent: -50, yPercent: -50 }); // Centra la bola en el trazo
+    // gsap.set("#text-path", { drawSVG: "0%" }); // Inicialmente oculta el trazo
+    // gsap.set(".ball", { xPercent: -50, yPercent: -50 }); // Centra la bola en el trazo
 
-    // Línea de tiempo para la animación
-    const timeline = gsap.timeline({
-      repeat: -1,
-      yoyo: true // Añadido efecto yoyo
-    });
+    // // Línea de tiempo para la animación
+    // const timeline = gsap.timeline({
+    //   repeat: -1,
+    //   yoyo: true // Añadido efecto yoyo
+    // });
 
-    // Dibuja el trazo del texto
-    timeline.to("#text-path", {
-      duration: 5,
-      drawSVG: "100%",
-      ease: "none"
-    });
+    // // Dibuja el trazo del texto
+    // timeline.to("#text-path", {
+    //   duration: 5,
+    //   drawSVG: "100%",
+    //   ease: "none"
+    // });
 
-    // Anima la bola para que siga el trazo del texto
-    timeline.to(".ball", {
-      duration: 5,
-      motionPath: {
-        path: "#text-path",
-        align: "#text-path",
-        alignOrigin: [0.5, 0.5]
-      },
-      ease: "none"
-    }, 0);
+    // // Anima la bola para que siga el trazo del texto
+    // timeline.to(".ball", {
+    //   duration: 5,
+    //   motionPath: {
+    //     path: "#text-path",
+    //     align: "#text-path",
+    //     alignOrigin: [0.5, 0.5]
+    //   },
+    //   ease: "none"
+    // }, 0);
   }
 
   scrollToSection(sectionId: string) {
